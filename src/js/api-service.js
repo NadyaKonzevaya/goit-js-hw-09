dba8eb3f908bcb232787bc479b1011feb282ea65
NadyaKonzevaya <borisova.nadya@gmail.com>
2022-05-18 14:03:10 +0300

    /

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -6,7 +6,7 @@
 
 // "https://pokeapi.co/api/v2/"
     // наш корневой адрес для запроса
-    const BASE = "http://pokeapi.salestock.net/api/v2/"
+    const BASE = "http://pokeapi.salestock.net/api/v2"
 function fetchPokemon(id) {
     const url = `${BASE}/pokemon/${id}`;
     return fetch(url)
