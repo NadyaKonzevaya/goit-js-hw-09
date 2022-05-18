8ce6a91661b5cb44f0a9997f30b42e3fc16a973c
NadyaKonzevaya <borisova.nadya@gmail.com>
2022-05-18 16:01:55 +0300

    возвращает назад покемонов

--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -2,5 +2,5 @@ export default function getRefs() {
     return  {
         cardContainer: document.querySelector(".js-card-container"),
         searchForm: document.querySelector(".js-search-form")
-}
+    }
 }
